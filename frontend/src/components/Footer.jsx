52d7b4783f2af3e9e388f1e0f3cfc12ac9d29922
salilgupta332 <salilgupta332@gmail.com>
2025-12-12 07:03:05 +0530

    JeninFiles Updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         {/* ---------- Left Section ---------- */}
         <div>
           <img className='mb-5 w-40' src={assets.logo} alt="" />
-          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         </div>
 
         {/* ---------- Center Section ---------- */}
